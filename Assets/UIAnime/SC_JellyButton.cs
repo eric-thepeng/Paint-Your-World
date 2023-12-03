eb8797b536394459b3faa9554689066da329fb53
Puluki <129189927+Puluki@users.noreply.github.com>
2023-12-03 17:13:27 -0500

    SomeUIUpdates
    needs more animation on colors and buttons

---
+++ Assets/UIAnime/SC_JellyButton.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SC_JellyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public float scaleFactor = 1.2f;
+    public float animationSpeed = 3f;
+
+    private Vector3 originalScale;
+    private bool isPressed = false;
+    private bool isHovering = false;
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        if (isPressed || isHovering)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, originalScale * scaleFactor, Time.deltaTime * animationSpeed);
+        }
+        else
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, originalScale, Time.deltaTime * animationSpeed);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovering = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovering = false;
+    }
+}

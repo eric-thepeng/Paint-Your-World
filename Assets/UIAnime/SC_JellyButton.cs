c68c87220dc97913e55b7dcb1262b78baf3b08d6
Puluki <129189927+Puluki@users.noreply.github.com>
2023-12-06 22:42:16 -0500

    updated animation and UI interactions
    but didn't link to specific functions like adjust the volume

--- Assets/UIAnime/SC_JellyButton.cs
+++ Assets/UIAnime/SC_JellyButton.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SC_JellyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 {

93b68464f05ba8f224cf4f2d8147f6c1a60aef8a
Peng Guo <errrrrricg@gmail.com>
2023-12-11 01:04:13 -0500

    camera manager, fix creature display

--- Assets/Peng Assets/PlaceableIdentifier/CameraManager.cs
+++ Assets/Peng Assets/PlaceableIdentifier/CameraManager.cs
@@ -40,19 +40,19 @@ public class CameraManager : MonoBehaviour
         float moveY = 0f;
 
         // Get input from WASD keys
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             moveY = 1f;
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             moveY = -1f;
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             moveX = -1f;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             moveX = 1f;
         }
